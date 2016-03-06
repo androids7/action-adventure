42ea3498d26cd2903e752625b808e5cb38d91cba
Rafael Madureira dos Santos <rafael.madureira.santos@gmail.com>
2016-03-06 18:54:32 -0300

    Add sprite animation

--- src/game.cpp
+++ src/game.cpp
@@ -14,7 +14,7 @@
 
 namespace {
     const int FPS = 50;
-    const int MAX_FRAME_TIME = 5 * 1000 / FPS;
+    const int MAX_FRAME_TIME = 1000 / FPS;
 }
 
 Game::Game() {
@@ -30,13 +30,17 @@ void Game::gameLoop() {
     Graphics graphics;
     Input input;
     SDL_Event event;
-    this->_player = Sprite(graphics, "content/sprites/MyChar.png", 0, 0, 16, 16, 100, 100);
+
+    this->_player = AnimatedSprite(graphics, "content/sprites/MyChar.png", 0, 0, 16, 16, 100, 100, 200);
+    this->_player.setupAnimations();
+    this->_player.playAnimation("RunLeft");
 
     int LAST_UPDATE_TIME = SDL_GetTicks();
 
     // Start the game loop
     while (true) {
         input.beginNewFrame();
+
         if (SDL_PollEvent(&event)) {
             if (event.type == SDL_KEYDOWN) {
                 if (event.key.repeat == 0) {
@@ -57,6 +61,7 @@ void Game::gameLoop() {
 
         const int CURRENT_TIME_MS = SDL_GetTicks();
         int ELAPSED_TIME_MS = CURRENT_TIME_MS - LAST_UPDATE_TIME;
+
         this->update(std::min(ELAPSED_TIME_MS, MAX_FRAME_TIME));
         LAST_UPDATE_TIME = CURRENT_TIME_MS;
 
@@ -74,6 +79,5 @@ void Game::draw(Graphics &graphics) {
 }
 
 void Game::update(float elapsedTime) {
-
+    this->_player.update(elapsedTime);
 }
-
