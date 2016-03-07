3a7c42daecfc0c25398d5d4d8b59ffbd0c3e41ec
Rafael Madureira dos Santos <rafael.madureira.santos@gmail.com>
2016-03-06 23:29:22 -0300

    Add movement to player

--- headers/player.h
+++ headers/player.h
@@ -2,7 +2,6 @@
 #define PLAYER_H
 
 #include "animatedsprite.h"
-#include <string>
 
 class Graphics;
 
@@ -15,11 +14,31 @@ public:
     void draw(Graphics &graphics);
     void update(float elapsedTime);
 
+    /**
+     * void moveLeft
+     * Moves the player left by -dx
+     */
+    void moveLeft();
+
+    /**
+     * void moveRight
+     * Moves the player right by dx
+     */
+    void moveRight();
+
+    /**
+     * void stopMoving
+     * Stops moving the player
+     */
+    void stopMoving();
+
     virtual void animationDone(std::string currentAnimation);
     virtual void setupAnimations();
 
 private:
+    float _dx, _dy;
 
+    Direction _facing;
 
 };
 
